1b87cb47084c9d9c9ff24378e99077b4685ff2d1
Lucas DUPUIS <dupuis@antadis.com>
2024-10-04 20:17:57 +0200

    mise en place de la CI

--- upload.sh
+++ upload.sh
@@ -3,10 +3,10 @@
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
 if [ "$branch" ==  "master" ]; then
-	/opt/Godot/Godot --export "HTML5" Exports/html5/index.html
-	zip -r Exports/html5.zip Exports/html5/
-	butler push Exports/html5.zip jammin/ludum-dare-52:html5
-	butler status jammin/ludum-dare-52:html5
+	/opt/Godot/Godot --export-release "Web" Exports/Web/index.html
+	zip -r Exports/web.zip Exports/Web/
+	butler push Exports/web.zip jammin/ludum-dare-56:html5
+	butler status jammin/ludum-dare-56:html5
 else
 	echo 'Commit on non-master branch: Running jshint on current changeset.'
 fi
