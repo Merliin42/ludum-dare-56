78b9d658edde5d4e8ace7e0c4f07f506e9992ab0
Lucas DUPUIS <dupuis@antadis.com>
2024-10-05 20:28:04 +0200

    upgrade menu

---
+++ GUI/level_up_menu.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+signal upgrade_choosed(value);
+
+@onready var attacks_description: Array = AttacksDescription.new().attacks_description;
+var upgrade_option := preload('res://GUI/upgrade_option.tscn');
+
+func _ready() -> void:
+	get_tree().paused = true;
+	for i in range(3):
+		var index = randi() % attacks_description.size();
+		var attack = attacks_description[index];
+		var upgrade_option_instanced = upgrade_option.instantiate();
+		if is_instance_of(upgrade_option_instanced, UpgradeOption):
+			upgrade_option_instanced.title = attack.title;
+			upgrade_option_instanced.description = attack.description;
+			upgrade_option_instanced.icon_path = attack.icon;
+			upgrade_option_instanced.id = attack.id;
+			upgrade_option_instanced.has_been_pressed.connect(_on_button_clicked);
+			$VBoxContainer.add_child(upgrade_option_instanced);
+
+func _on_button_clicked(value: String):
+	upgrade_choosed.emit(value);
+	get_tree().paused = false;
+	queue_free();
--- main.gd
+++ main.gd
@@ -1,11 +1,21 @@
 extends Node
 
+var level_up_menu := preload('res://GUI/level_up_menu.tscn');
 var player_xp = 0;
-var next_level = 100;
+var next_level = 1;
 
 func _on_character_died() -> void:
 	get_tree().quit();
 
-func _on_character_xp_obtained(amount:Variant) -> void:
+func _on_character_xp_obtained(amount: Variant) -> void:
 	player_xp += amount;
-	$CanvasLayer/XPBar.value = player_xp * 100 / next_level;	
+	$CanvasLayer/XPBar.value = player_xp * 100 / next_level;
+	if player_xp >= next_level:
+		var level_up_menu_instanced = level_up_menu.instantiate();
+		level_up_menu_instanced.connect('upgrade_choosed', _on_upgrade_choosed);
+		$CanvasLayer.add_child(level_up_menu_instanced);
+
+	
+func _on_upgrade_choosed(value: String):
+	print(value);
+	pass ;
