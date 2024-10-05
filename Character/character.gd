08ecadcebb7d94d9e152e1123d6938d598db6d92
Lucas DUPUIS <dupuis@antadis.com>
2024-10-05 14:43:38 +0200

    player take damages

--- Character/character.gd
+++ Character/character.gd
@@ -1,12 +1,34 @@
-extends CharacterBody2D
+extends CharacterBody2D;
 
-const SPEED = 200;
+signal damage_taken(new_health);
+
+@export var speed = 200;
+@export var health = 10;
+@export var i_frame_enable := false;
+
+var i_frame := false;
 
 func _physics_process(delta: float) -> void:
 	velocity = Vector2(0, 0);
 	velocity.x = Input.get_axis('character_left', 'character_right');
 	velocity.y = Input.get_axis('character_up', 'character_down');
 	velocity = velocity.normalized();
-	velocity *= SPEED;
- 
+	velocity *= speed;
+
 	move_and_slide()
+
+func take_damage(amount: int) -> void:
+	if i_frame && i_frame_enable:
+		return ;
+	i_frame = true;
+	$AnimationPlayer.play('take_damage');
+	health -= amount;
+	emit_signal('damage_taken', health);
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == 'take_damage':
+		i_frame = false;
+
+func _on_mob_detector_body_entered(body:Node2D) -> void:
+	if is_instance_of(body, Enemy):
+		take_damage(body.damages);
--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,15 +1,17 @@
-extends Area2D 
+class_name Enemy
+extends CharacterBody2D
 
 @export var speed := 100;
 @export var health := 10;
 @export var damages := 10;
 
 func _process(delta: float) -> void:
-	# var center = get_viewport_rect().size / 2;
 	var camera = get_viewport().get_camera_2d()
 
 	if camera:
-		var center_ajusted = camera.global_position 
-		position = position.move_toward(center_ajusted, delta * speed);
-	
-	print(camera.global_position);
+		velocity = camera.global_position - self.global_position;
+		velocity = velocity.normalized();
+		velocity = velocity * speed;
+		move_and_slide();
+
+		# position = position.move_toward(center_ajusted, delta * speed);
