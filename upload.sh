005297d6fcfdd701a13866357909968bc0987308
Lucas DUPUIS <dupuis@antadis.com>
2024-10-07 23:07:39 +0200

    modification du lien d'upload

--- upload.sh
+++ upload.sh
@@ -5,8 +5,8 @@ branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 if [ "$branch" ==  "master" ]; then
 	/opt/Godot/Godot --export-release "Web" Exports/Web/index.html
 	zip -r Exports/web.zip Exports/Web/
-	butler push Exports/web.zip jammin/ludum-dare-56:html5
-	butler status jammin/ludum-dare-56:html5
+	butler push Exports/web.zip jammin/taupattack:html5
+	butler status jammin/taupattack:html5
 else
 	echo 'Commit on non-master branch: Running jshint on current changeset.'
 fi
